e22780d7cf7b7a120136f4fa00269c11436bea69
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 12:56:24 +0100

    try 2 for commit basic layout application-class and databases

---
+++ src/main/java/logic/MoneyTrackerApplication.java
@@ -0,0 +1,26 @@
+package logic;
+
+
+public class MoneyTrackerApplication {
+    public MoneyTrackerApplication() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    private void run() {
+        /*\
+         TODO : split run into state machine coupled with buttons/use-cases
+                    - create group
+                    - add people to group
+                    - add ticket : price, person who paid, people who are in debt, how much each debts is, ...
+                        - > 'update depts'
+                    - update debts of group members
+                    - someone pays
+                        - > 'update debts'
+
+         */
+    }
+}
