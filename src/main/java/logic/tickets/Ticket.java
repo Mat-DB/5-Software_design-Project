5ed4dc30ff007944f148081a5ae80559303ecdbe
Matthias De Beukelaer <matthiasdb2912@gmail.com>
2024-01-23 13:47:49 +0100

    Added some java doc.

--- src/main/java/logic/tickets/Ticket.java
+++ src/main/java/logic/tickets/Ticket.java
@@ -5,14 +5,43 @@ import logic.tickets.TicketSplit.TypeSplit;
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * The interface Ticket.
+ */
 public interface Ticket extends TicketInfo {
+    /**
+     * Gets balances.
+     *
+     * @return the balances
+     */
     HashMap<Integer, Double> getBalances();
 
+    /**
+     * Gets debtors.
+     *
+     * @return the debtors
+     */
     Set<Integer> getDebtors();
 
+    /**
+     * Add payment.
+     *
+     * @param user    the user
+     * @param payment the payment
+     */
     void addPayment(int user, double payment);
 
+    /**
+     * Is even boolean.
+     *
+     * @return the boolean
+     */
     boolean isEven();
 
+    /**
+     * Gets split type.
+     *
+     * @return the split type
+     */
     TypeSplit getSplitType();
 }
--- src/main/java/logic/tickets/TicketSplit/DecoratorTicketSplit.java
+++ src/main/java/logic/tickets/TicketSplit/DecoratorTicketSplit.java
@@ -7,21 +7,28 @@ import logic.tickets.TicketEvents.TypeEvents;
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * A decorator for an event ticket to make the ticket splittable.
+ */
 public abstract class DecoratorTicketSplit implements Ticket {
     protected TicketInfo eventTicket;
 
     /**
-     * balance of debtors in ticket.
-     * negative balance means debt
+     * Balance of debtors in ticket.
+     * Negative balance means debt.
      */
     protected HashMap<Integer, Double> balances;
 
     /**
-     * list of people with debt to payer
+     * List of people with debt to payer.
      */
     protected Set<Integer> debtors;
     protected boolean balancesInitialized = false;
 
+    /**
+     * Constructor for the decorator.
+     * @param eventTicket event ticket to decorate as splittable ticket.
+     */
     public DecoratorTicketSplit (TicketInfo eventTicket) {
         this.eventTicket = eventTicket;
         balances = new HashMap<>();
@@ -29,21 +36,26 @@ public abstract class DecoratorTicketSplit implements Ticket {
     }
 
 
+    /**
+     * Gets event ticket.
+     *
+     * @return the event ticket
+     */
     public TicketInfo getEventTicket() {
         return eventTicket;
     }
 
-    // ToDo: add java doc or remove
-
     /**
-     * @return
+     * Returns the debtors.
+     * @return debtors
      */
     public Set<Integer> getDebtors() {
         return debtors;
     }
 
     /**
-     * @return
+     * Returns the ticket name.
+     * @return name
      */
     @Override
     public String getName() {
@@ -51,7 +63,8 @@ public abstract class DecoratorTicketSplit implements Ticket {
     }
 
     /**
-     * @return
+     * Returns the ticket total.
+     * @return total
      */
     @Override
     public double getTotal() {
@@ -59,7 +72,8 @@ public abstract class DecoratorTicketSplit implements Ticket {
     }
 
     /**
-     * @return
+     * Returns the ID of the person who paid the ticket. (Ask the user controller who this is.)
+     * @return payer
      */
     @Override
     public int getWhoPaid() {
@@ -67,9 +81,9 @@ public abstract class DecoratorTicketSplit implements Ticket {
     }
 
     /**
-     * @return
+     * Returns the balance of the ticket, who needs to pay who and how much.
+     * @return balance
      */
-
     public HashMap<Integer, Double> getBalances() {
         System.out.println(this.getClass() + ", balance: " + balances);
         System.out.println(this.getClass() + ", toString: " + toString());
@@ -77,13 +91,18 @@ public abstract class DecoratorTicketSplit implements Ticket {
     }
 
     /**
-     * @return
+     * Returns the type of event.
+     * @return event type
      */
     @Override
     public TypeEvents getEventType() {
         return eventTicket.getEventType();
     }
 
+    /**
+     * Overwritten the toString implementation.
+     * @return string
+     */
     @Override
     public String toString() {
         return getEventType() + " TicketInfo{" +
@@ -94,6 +113,11 @@ public abstract class DecoratorTicketSplit implements Ticket {
                 '}';
     }
 
+    /**
+     * Add an extra payment if one is forgotten.
+     * @param user    the user
+     * @param payment amount
+     */
     @Override
     public void addPayment(int user, double payment) {
         double userCurrentBalance = balances.get(user);
@@ -104,6 +128,10 @@ public abstract class DecoratorTicketSplit implements Ticket {
 
     }
 
+    /**
+     * The isEven function checks if the balances of all users in the group are even.
+     * @return True if all the balances are 0
+     */
     @Override
     public boolean isEven() {
         for (double debt : balances.values()) {
